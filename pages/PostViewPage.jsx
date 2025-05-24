b41ffaf3cdf9857de6af4aa0422701a409396144
YeJun Kim <147353793+arane152@users.noreply.github.com>
2025-05-25 02:37:26 +0900

    YeJun-Device.jsx-05250237

---
+++ pages/PostViewPage.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+import Device from "../src/layouts/Device";
+import Header from "../src/layouts/Header";
+import Gnb from "../src/layouts/Gnb";
+
+import PostImage from "../src/components/PostImage";
+import PostConatiner from "../src/modules/PostContainer";
+import PostMenuConatiner from "../src/modules/PostMenuContainer";
---
+++ src/layouts/Device.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 393px;
+    height: 852px;
+    flex-direction: column;
+`
+
+function Device(props) {
+    return (
+        <Wrapper></Wrapper>
+    )
+}
+
+export default Device
