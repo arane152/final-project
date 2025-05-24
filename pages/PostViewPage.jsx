06ff75ee024cbe0c16b260d7bfc49cc8cc1b19b1
YeJun Kim <147353793+arane152@users.noreply.github.com>
2025-05-25 04:13:54 +0900

    YeJun-Clean PostViewPage.jsx-05250413

--- pages/PostViewPage.jsx
+++ pages/PostViewPage.jsx
@@ -1,11 +1,4 @@
-import styled from "styled-components";
-
 import Device from "../src/layouts/Device";
-import Header from "../src/layouts/Header";
-import Gnb from "../src/layouts/Gnb";
-
-
-import MainBtn from "../src/components/MainBtn";
 import PostImage from "../src/components/PostImage";
 import PostConatiner from "../src/modules/PostContainer";
 import PostMenuConatiner from "../src/modules/PostMenuContainer";
