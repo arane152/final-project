91d1ef9c9e777cdfd06824748ee0871e2214eed5
jispark <popopo0128@naver.com>
2025-05-25 13:29:19 +0900

    "포스트작성페이지" 모듈 제작

--- src/modules/InfoArea.jsx
+++ src/modules/InfoArea.jsx
@@ -7,7 +7,7 @@ import ToggleBtn from "../components/ToggleBtn";
 
 const StyledWrapper = styled.div`
     width: 393px;
-    height: 570px;
+    height: 574px;
     background-color: #ffffff;
     display: flex;
     flex-direction: column;
@@ -18,18 +18,29 @@ const Container = styled.div`
     width: 353px;
     display: flex;
     flex-direction: column; 
-    gap: 12px;
+    gap: 20px;
     padding-top: 16px;
+`
+
+const ImgBtn = styled.button`
+    width: 70px;
+    height: 70px;
+    border-radius: 8px;
+    background-color: #F8F8F8;
+    color: #AAAAAA;
+    border: none;
+    text-align: center;
 `
 
 function InfoArea(props) {
     return(
     <StyledWrapper>
         <Container>
-            <InfoBox title="제목"><TextInput></TextInput></InfoBox>
+            <InfoBox title="제목"><TextInput placeholder="제목을 입력해주세요."></TextInput></InfoBox>
             <InfoBox title="수령장소"><TextInput placeholder="수령장소를 입력해주세요."></TextInput></InfoBox>
             <InfoBox title="마감시간"><TextInput placeholder="마감시간을 입력해주세요."></TextInput></InfoBox>
             <InfoBox title="내용"><TextInput height={"108px"} placeholder="내용을 입력해주세요."></TextInput></InfoBox>
+            <InfoBox title="메뉴사진"><ImgBtn>+</ImgBtn></InfoBox>
             <InfoBox title="계좌번호"><TextInput placeholder="계좌번호를 입력해주세요."></TextInput></InfoBox>
             <InfoBox title={<>선입금<br />필수여부</>}>
                 <ToggleBtn width='142' text="자유"></ToggleBtn>
