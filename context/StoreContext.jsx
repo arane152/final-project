73c2f11078b86675f4640197bd82a20cbd0439e6
tlaxotjq <1212sts@naver.com>
2025-06-03 23:35:55 +0900

    Taeseob-"Context-파이어베이스해제"-0603
    firebase 사용량 조절을 위한
    데이터불러오기 해제
    
    파일 내부에서 데이터관리방식으로 변경

--- context/StoreContext.jsx
+++ context/StoreContext.jsx
@@ -8,13 +8,19 @@ const StoreProvider = ({ children }) => {
   const [storeData, setStore] = useState([])
 
   useEffect(() => {
-    let tempData = [];
-    db.collection('store').get().then(function (qs) {
-      qs.forEach(function (doc) {
-        tempData.push(doc.data());
-      });
-      setStore( tempData);
-    });
+    // let tempData = [];
+    // db.collection('store').get().then(function (qs) {
+    //   qs.forEach(function (doc) {
+    //     tempData.push(doc.data());
+    //   });
+    //   setStore(tempData);
+    // });
+    setStore([
+      {id: 1, name: "청년다방", categoryId: 5, minPrice: "12000"},
+      {id: 2, name: "홍콩반점", categoryId: 2, minPrice: "20000"},
+      {id: 3, name: "스시로", categoryId: 11, minPrice: "15000"},
+      {id: 4, name: "테스트", categoryId: 1, minPrice: "9999"},
+    ])
   }, []);
 
   return (
--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -10,13 +10,24 @@ const UserProvider = ({ children }) => {
   const [nowuser, setUser] = useState(null);
 
   useEffect(() => {
-    let tempData = [];
-    db.collection('user').get().then(function (qs) {
-      qs.forEach(function (doc) {
-        tempData.push(doc.data());
-      });
-      setUserList(tempData);
-    });
+    // let tempData = [];
+    // db.collection('user').get().then(function (qs) {
+    //   qs.forEach(function (doc) {
+    //     tempData.push(doc.data());
+    //   });
+    //   setUserList(tempData);
+    // });
+    setUserList([
+      {userId: "1", name: "홍길동", location: "1기숙사", accountNumber: "00000000000",
+        profile: "https://mblogthumb-phinf.pstatic.net/MjAyMjA3MTVfMjQz/MDAxNjU3ODcwNzkwNDI0.kcOc-7e2R90i8Wf36bIfvbX9Zm1YWj-167gJN-qBdkgg.9Uwu9QvqoTXtJb-Mm3qM2XnGRKINh0ZAwvc90o3-NBQg.JPEG.wst1/Screenshot%EF%BC%BF20220705%EF%BC%8D154328%EF%BC%BFNAVER.jpg?type=w800"
+      },
+      {userId: "2", name: "사용자", location: "2기숙사", accountNumber: "00000000000",
+        profile: ""
+      },
+      {userId: "3", name: "누군가", location: "정왕본동", accountNumber: "00000000000",
+        profile: ""
+      },
+    ]);
   }, []);
 
   useEffect(() => {
