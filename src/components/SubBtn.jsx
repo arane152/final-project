40ebc27f71b52525e45216f7f11c06faedc0592b
ParkYein0413 <pyi0413@naver.com>
2025-06-07 12:16:03 +0900

    subBtn Onclick 추가

--- src/components/SubBtn.jsx
+++ src/components/SubBtn.jsx
@@ -9,6 +9,7 @@ const BtnWrapperDefault = styled.div`
     justify-content: center;
     align-items: center;
     padding: 0px 11px;
+    cursor: pointer;
 `
 
 const BtnWrapperStroke = styled(BtnWrapperDefault)`
@@ -33,43 +34,45 @@ const BtnTextStroke = styled(BtnText)`
 `
 
 function SubBtn(props) {
-    // props.type : 버튼 타입 (default : "" / stroke : "storke" / stroke-plus : "stroke-plus" / next : "next")
+    // props.type : 버튼 타입 (default : "" / stroke : "storke" / stroke-plus : "stroke-plus" / next : "next" / grey)
     // props.text : 버튼 내용
+    // props.onClick : 버튼 클릭 이벤트
+
     if (props.type == "stroke") {
         return (
-            <BtnWrapperStroke>
+            <BtnWrapperStroke onClick={props.onClick}>
                 <BtnTextStroke>{props.text}</BtnTextStroke>
             </BtnWrapperStroke>
-        )
+        );
     }
     else if (props.type == "stroke-plus") {
         return (
-            <BtnWrapperStroke>
+            <BtnWrapperStroke onClick={props.onClick}>
                 <BtnTextStroke>{props.text} &nbsp;+</BtnTextStroke>
             </BtnWrapperStroke>
-        )
+        );
     }
     else if (props.type == "next") {
         return (
-            <BtnWrapperDefault>
+            <BtnWrapperDefault onClick={props.onClick}>
                 <BtnText>{props.text} &nbsp;→</BtnText>
             </BtnWrapperDefault>
-        )
+        );
     }
     else if (props.type == "grey") {
         return (
-            <BtnWrapperGreyStroke>
+            <BtnWrapperGreyStroke onClick={props.onClick}>
                 <BtnTextStroke>{props.text}</BtnTextStroke>
             </BtnWrapperGreyStroke>
-        )
+        );
     }
     else {
         return (
-            <BtnWrapperDefault>
+            <BtnWrapperDefault onClick={props.onClick}>
                 <BtnText>{props.text}</BtnText>
             </BtnWrapperDefault>
-        )
+        );
     }
 }
 
-export default SubBtn
+export default SubBtn;
