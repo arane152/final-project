7aed610268505a25902edb15d790ffd66690b341
tlaxotjq <1212sts@naver.com>
2025-06-10 21:54:50 +0900

    "post(1) 삭제에 따른 알림목업 수정"

--- pages/AlarmPage.jsx
+++ pages/AlarmPage.jsx
@@ -31,22 +31,22 @@ function AlarmPage(props) {
     if (!nowuser) return; //nowuser 랜더링중 오류방지
 
     //현재 사용자의 postId가 "postId(1)"인 Post만 필터링
-    const filteredUserPost = 
-    postData.filter((item) => item.postId == "postId(1)");
+  const filteredUserPost = 
+    postData.filter((item) => item.postId == "1749558559258");
     setNowPost(filteredUserPost);
-  }, [postData]);
+  }, []);
 
 
   //알림목록불러오기
   const [notData, setNotData] = useState([])
   useEffect(() => {
     setNotData([
-      { notId: 1, type: "partyendCaptain", postId: "postId(1)", userId: 1, time: "방금" },
-      { notId: 2, type: "totalend", postId: "postId(1)", userId: 1, time: "6분전"},
-      { notId: 3, type: "partyrequest", postId: "postId(1)", userId: 1, btntype:"stroke", membername: "신청자", time: "8분전" , menuname: "떡볶이", menucount: "1", menuprice: "12000"},
-      { notId: 4, type: "timeend", postId: "postId(1)", userId: 1, time: "10분전" },
-      { notId: 5, type: "partyrequest", postId: "postId(1)", userId: 1, membername: "아무개", time: "30분전", menuname: "공깃밥", menucount: "1", menuprice: "1000" },      
-      { notId: 6, type: "partyendCaptain", postId: "postId(1)", userId: 1, time: "1일전" },
+      { notId: 1, type: "partyendCaptain", postId: "1749558559258", userId: 1, time: "방금" },
+      { notId: 2, type: "totalend", postId: "1749558559258", userId: 1, time: "6분전"},
+      { notId: 3, type: "partyrequest", postId: "1749558559258", userId: 1, btntype:"stroke", membername: "신청자", time: "8분전" , menuname: "떡볶이", menucount: "1", menuprice: "12000"},
+      { notId: 4, type: "timeend", postId: "1749558559258", userId: 1, time: "10분전" },
+      { notId: 5, type: "partyrequest", postId: "1749558559258", userId: 1, membername: "아무개", time: "30분전", menuname: "공깃밥", menucount: "1", menuprice: "1000" },      
+      { notId: 6, type: "partyendCaptain", postId: "1749558559258", userId: 1, time: "1일전" },
     ])
   }, []);
 
